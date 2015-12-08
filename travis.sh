f4a18ee4bda11073235c35829cc0edfe0927fc75
Jaime <jaimebeneytezduran@gmail.com>
2015-12-08 16:38:24 +0000

    feat(travis): avoid system check

--- travis.sh
+++ travis.sh
@@ -5,12 +5,13 @@ npm cache clear && rm -rf node_modules && npm install
 npm install -g grunt-cli bower
 
 # checks node, npm, bower & grunt-cli version
-./bin/systemcheck.sh
-check=$?
-if [[ $check > 0 ]]
-then
-    exit $check;
-fi
+# ./bin/systemcheck.sh
+# check=$?
+# if [[ $check > 0 ]]
+# then
+    # exit $check;
+# fi
+
 
 # ci tasks
 # bower cache clean
